b5d07ca2b64fbd60dc917247610e4bad50aedc42
Edgard Matthijs <edgardm@mint>
2019-11-14 12:01:17 +0100

    another commit

--- bootstrap.sh
+++
@@ -1,8 +0,0 @@
-intltoolize --force --automake
-autoreconf --force --install --symlink
-
-if test "x$NOCONFIGURE" = "x"; then
-    CFLAGS="-g -O0" ./configure --sysconfdir=/etc --localstatedir=/var "$@"
-    make clean
-fi
-
