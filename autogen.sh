d964ac3c2d39172e0c228de9a873e341e6374226
gamegard68 <ematthijs@skynet.be>
2019-11-18 15:35:42 +0100

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,12 +1,5 @@
 #!/bin/sh -e
 # autogen.sh - Run this to generate all the initial makefiles, etc.
 #
-#  Copying and distribution of this file, with or without modification,
-#  are permitted in any medium without royalty provided the copyright
-#  notice and this notice are preserved.  This file is offered as-is,
-#  without any warranty.
-
-glib-gettextize --copy --force
-# intltoolize --copy --force --automake
 
 autoreconf -f -i && automake -a -f
