3f52deb0566f657b248728049a2a8c06b99b78fd
gamegard68 <ematthijs@skynet.be>
2019-11-18 23:04:46 +0100

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -2,4 +2,18 @@
 # autogen.sh - Run this to generate all the initial makefiles, etc.
 #
 
-autoreconf -f -i && automake -a -f
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common"
+    exit 1
+}
+
+# Ensure that m4 directory exists
+if ! test -d $srcdir/m4; then
+	mkdir $srcdir/m4
+fi
+
+REQUIRED_AUTOMAKE_VERSION=1.9 . gnome-autogen.sh
+
